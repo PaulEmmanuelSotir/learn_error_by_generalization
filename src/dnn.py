Fixed computation graph of learn_error_by_generalization.py Implemented import_and_reload, graph_replace and extract_update_dict in utils.py Made various improvements in learn_error_by_generalization.py

src/dnn.py
@@ -65,16 +65,15 @@ class DNN(object):
             for i, in_size, out_size in zip(range(1, len(self.layers)), self.layers[:-1], self.layers[1:]):
                 layer = self._dense_layer(layer, [in_size, out_size], 'layer_{}'.format(i), self.batch_norm, self.weight_init, self.activation_fn, self.dropout)
             self.logits = self._dense_layer(layer, [self.layers[-1], self.n_classes], 'output_layer', self.batch_norm, self.weight_init)
-            with tf.variable_scope('output_layer'):
-                self.probs = tf.nn.softmax(self.logits, name='probs')
+            self.probs = tf.nn.softmax(self.logits, name='probs')
 
     def _dense_layer(self, x, shape, name, batch_norm, init, activation_fn=None, keep_prob=1.):
         with tf.variable_scope(name):
             self.weights.append(tf.get_variable(initializer=init(shape), name='w'))
             self.biases.append(tf.get_variable(initializer=tf.truncated_normal([shape[1]]) if shape[1] > 1 else 0., name='b'))
-            logits = tf.add(tf.matmul(x, self.weights[-1]), self.biases[-1])
+            logits = tf.add(tf.matmul(x, self.weights[-1]), self.biases[-1], name='logits' if not batch_norm else 'logits_no_bn')
             if batch_norm:
-                logits = tf.layers.batch_normalization(logits, training=self.training, name='batch_norm')
+                logits = tf.layers.batch_normalization(logits, training=self.training, name='logits')
             dense = activation_fn(logits) if activation_fn is not None else logits
-            dense = tf.nn.dropout(dense, keep_prob, name='logits')
+            dense = tf.nn.dropout(dense, keep_prob, name='output')
         return dense

src/learn_error_by_generalization.py
@@ -4,8 +4,11 @@
 Learn error by explicit generalization
 Learn to generalize explicitly by learning error vector on testset batch
 """
+import numpy as np
 import tensorflow as tf
+from tensorflow.python import keras
 import tensorflow.contrib.graph_editor as ge
+from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.datasets import cifar10, cifar100
 
 import utils
@@ -18,14 +21,18 @@ N_CLASSES = 100 if USE_CIFAR100 else 10
 
 hyperparameters_config = {
     'lr': {'default': 1e-3, 'metavar': '-l', 'type': float, 'help': 'learning rate'},
-    'sub_lr': {'default': 5e-2, 'metavar': '-r', 'type': float, 'help': 'sub-learning rate'},
-    'test_batch_size': {'default': 128, 'metavar': '-b', 'type': int, 'help': 'Training batch size'},
-    'sub_steps': {'default': 1, 'type': int, 'help': 'Number of gradient descent steps to learn error parameter'},
     'steps': {'default': 64, 'metavar': '-s', 'type': int, 'help': 'Number of gradient descent steps to learn error parameter'},
-    'train_batch_size': {'default': 256, 'metavar': '-b', 'type': int, 'help': 'Test batch size'},
-    'trained_model_dir': {'default': '/home/pes/deeplearning/generalization_training/train_1/', 'type': str, 'help': 'Tensorflow pretrained model directory'},
-    'save_dir': {'default': '/home/pes/deeplearning/generalization_training/egt_1/', 'type': str, 'help': 'Tensorflow model save directory'}
+    'batch_size': {'default': 128, 'metavar': '-b', 'type': int, 'help': 'Training batch size'},
+    'sub_lr': {'default': 5e-2, 'type': float, 'help': 'sub-learning rate'},
+    'sub_steps': {'default': 1, 'type': int, 'help': 'Number of gradient descent steps to learn error parameter'},
+    'sub_beta1': {'default': 0.9, 'type': float, 'help': 'Beta 1 parameter of Adam sub optimizer'},
+    'sub_epsilon': {'default': keras.backend.epsilon(), 'type': float, 'help': 'Epsilon parameter of Adam sub optimizer'},
+    'sub_batch_size': {'default': 256, 'type': int, 'help': 'Test batch size'},
+    'save_dir': {'default': '/home/pes/deeplearning/generalization_training/egt_1/', 'type': str, 'help': 'Tensorflow model save directory'},
+    'trained_model_dir': {'default': '/home/pes/deeplearning/generalization_training/train_1/', 'type': str, 'help': 'Tensorflow pretrained model directory'}
 }
+
+__all__ = ['explicit_generalization_training', 'build_update_model', 'learn_error_by_generalization', 'evaluate']
 
 
 def explicit_generalization_training(hp, dataset):
@@ -40,70 +47,88 @@ def explicit_generalization_training(hp, dataset):
         update_model = build_update_model(hp)
 
         # Train the update model to minimize generalization error by finding the error vector for which weight updates imply the best score on trainset
-        learn_error_by_generalization(sess, update_model)
+        learn_error_by_generalization(sess, update_model, dataset)
 
 
 def build_update_model(hp):
-    with tf.variable_scope('EGT'):
+    with tf.variable_scope('DNN/EGT', reuse=tf.AUTO_REUSE):
         # TODO: support for batch norm parameters?
         # Compute loss on test batch from 'learnable_error' parameter and actual prediction
         # Note that learnable_error is initialized to zero, which means the intial update is equivalent to making NN learn its own output
-        test_X = tf.get_default_graph().get_tensor_by_name("DNN/X")
+        g = tf.get_default_graph()
+        test_X_ph = g.get_operation_by_name("DNN/X")
         error_init = tf.zeros_initializer()
-        learnable_error = tf.get_variable('learnable_error', initializer=error_init, shape=[None])
-        trained_variables = [v for v in tf.global_variables() if v.name[-1] == 'w' or v.name[-1] == 'b']
-        logits = tf.get_default_graph().get_tensor_by_name("DNN/logits")
+        trained_variables = [v for v in tf.global_variables() if v.name[:4] == 'DNN/']
+        learnable_error = tf.get_variable('learnable_error', initializer=error_init, shape=[hp['batch_size'], N_CLASSES])
+        logits = g.get_tensor_by_name('DNN/output_layer/logits/batchnorm/add_1:0')
+        probs = g.get_tensor_by_name("DNN/probs:0")
         log_probs = tf.nn.log_softmax(logits, name='log_probs')
-        test_loss = tf.reduce_sum(log_probs * tf.nn.softmax(logits + learnable_error))
+        new_probs = tf.nn.softmax(logits + learnable_error, name='new_loss')
+        test_loss = tf.reduce_sum(log_probs * new_probs, name='test_loss')
 
         # Build updated graph
-        replacements = {}
-        for sub_grad,  w in zip(tf.gradients(test_loss, trained_variables), trained_variables):
-            replacements[w] = w - hp['sub_lr'] * sub_grad
-        ge.graph_replace(test_loss, replacements, dst_scope='EGT/UpdatedDNN', src_scope='DNN')
+        train_X_ph = tf.placeholder(tf.float32, test_X_ph.outputs[0].get_shape(), name='X')
+        opt = Adam(lr=hp['sub_lr'], beta_1=hp['sub_beta1'], epsilon=hp['sub_epsilon'])
+        with tf.variable_scope('adam_updates'):
+            sub_updates = opt.get_updates(test_loss, trained_variables)
+        replacements = utils.extract_update_dict(sub_updates)
+        replacements[test_X_ph.outputs[0]] = train_X_ph
 
+    utils.graph_replace(test_loss, replacements, dst_scope='EGT/UpdatedDNN/', src_scope='DNN/')
+
+    with tf.variable_scope('EGT/'):
         # Compute loss of updated graph on train batch
-        train_x = tf.get_default_graph().get_tensor_by_name("EGT/UpdatedDNN/X")
-        train_y = tf.get_default_graph().get_tensor_by_name("EGT/UpdatedDNN/y")
-        Updated_logits = tf.get_default_graph().get_tensor_by_name("EGT/UpdatedDNN/logits")
-        generalization_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Updated_logits, labels=train_y), name='generalization_loss')
+        train_y_ph = tf.placeholder(tf.int32, [None], name='y')
+        updated_logits = g.get_tensor_by_name("EGT/UpdatedDNN/output_layer/logits/batchnorm/add_1:0")
+        generalization_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=updated_logits, labels=train_y_ph), name='generalization_loss')
 
         # We learn 'learnable_error' by backpropagating from 'generalization_loss' through sub-SGD update on test_loss
         # Note that this step is computationally expensive as this gradient computation needs second order derivative w.r.t model parameters
         # Thus, you either need to keep model capacity low or apply this technique on top layers of your model (TODO: implement this with hessian approximation?)
-        optimize = tf.group(*[tf.assign_sub(e, hp['lr'] * grad)
-                              for grad, e in zip(tf.gradients(generalization_loss, learnable_error), learnable_error)], name='optimize')
-    return (test_X, train_x, train_y), generalization_loss, test_loss, optimize, error_init
+        lr = tf.constant(hp['lr'], name='lr')
+        meta_gradients = tf.gradients(generalization_loss, learnable_error)
+        meta_optimize = tf.assign_sub(learnable_error, lr * meta_gradients[0], name='optimize')
+
+    return (test_X_ph, train_X_ph, train_y_ph), generalization_loss, test_loss, probs, new_probs, meta_optimize, error_init
 
 
-def learn_error_by_generalization(sess, update_model):
-    (placeholders, generalization_loss, test_loss, optimize, error_init) = update_model
-    (test_X, train_x, train_y) = placeholders
+def learn_error_by_generalization(sess, update_model, dataset):
+    (placeholders, generalization_loss, test_loss, probs, new_probs, meta_optimize, error_init) = update_model
+    (test_X_ph, train_X_ph, train_y_ph) = placeholders
+    (train_X, train_y), (test_X, test_y) = dataset
 
-    mean_test_acc = 0
-    for step, batch_size, (batch_x, batch_y) in utils.batch(hp['batch_size'], train_x, train_y, test_X, shuffle=True):
+    mean_test_acc, mean_new_test_acc = 0, 0
+    for step, test_batch_size, batch_x_test, batch_y_test in utils.batch(hp['batch_size'], test_X, test_y, fixed_size_batches=True):
         print('\n' + '-' * 80 + '\nStep %03d/%03d' % (step + 1, hp['step']))
         # Initialize error vector
         sess.run(error_init)
         # Train it
         mean_loss, mean_sub_loss = 0, 0
-        for sub_step, batch_size, (batch_x, batch_y) in utils.batch(hp['batch_size'], train_x, train_y, test_X, shuffle=True):
-            _, loss, sub_loss = sess.run([optimize, generalization_loss, test_loss], feed_dict={test_X:, train_x: , train_y: })
+        for sub_step, _, (batch_x, batch_y) in utils.batch(hp['sub_batch_size'], train_X, train_y, shuffle=True, fixed_size_batches=True):
+            result = sess.run([meta_optimize, generalization_loss, test_loss, probs, new_probs],
+                              feed_dict={test_X_ph: batch_x_test, train_X_ph: batch_x, train_y_ph: batch_y})
+            _, loss, sub_loss, test_probs, test_new_probs = result
+            mean_loss += loss / hp['sub_batch_size']
+            mean_sub_loss += sub_loss / hp['sub_batch_size']
             if sub_step >= hp['sub_step']:
                 break
         # Evaluate model accuracy on testset batch given trained error and DNN prediction
-        test_acc = evaluate(update_model, )
+        test_acc, new_test_acc = evaluate(test_probs, test_new_probs, batch_y_test)
         print('\tgeneralization_loss=%2.5f\tsub_loss=%2.5f\ttest_acc=%3.4f' % (mean_loss, mean_sub_loss, test_acc))
+        mean_test_acc += test_acc / hp['batch_size']
+        mean_new_test_acc += new_test_acc / hp['batch_size']
         if step >= hp['step']:
             break
-    print('\nTraining of error vectors done:\tmean_test_acc=%3.4f' % (mean_test_acc))
+    print('\nTraining of error vectors done:\tmean_test_acc=%3.4f\mean_new_test_acc=%3.4f' % (mean_test_acc, mean_new_test_acc))
 
 
-def evaluate(update_model, testset):
-    pass
+def evaluate(probs, new_probs, batch_y_test):
+    test_acc = np.sum(np.equal(np.argmax(probs, axis=1), batch_y_test))
+    new_test_acc = np.sum(np.equal(np.argmax(new_probs, axis=1), batch_y_test))
+    return test_acc, new_test_acc
 
 
 if __name__ == '__main__':
-    hp = utils.hyperparameters_from_args(hyperparameters_config, description='Explicit generalization training of fully connected neural network on CIFAR-100')
+    hp = vars(utils.hyperparameters_from_args(hyperparameters_config, description='Explicit generalization training of fully connected neural network on CIFAR-100'))
     dataset = utils.load_cifar(USE_CIFAR100)
     explicit_generalization_training(hp, dataset)

src/train_dnn.py
@@ -14,17 +14,18 @@ __all__ = ['build_graph', 'train']
 
 # Hyperparameters (argparse configuration)
 hyperparameters_config = {
-    '--lr': {'default': 1e-4, 'metavar': '-l', 'type': float, 'help': 'learning rate'},
-    '--l2_reg': {'default': 5e-4, 'metavar': '-r', 'type': float, 'help': 'L2 regularization factor'},
+    '--lr': {'default': 0.067133, 'metavar': '-l', 'type': float, 'help': 'learning rate'},
+    '--l2_reg': {'default': 0.03232, 'metavar': '-r', 'type': float, 'help': 'L2 regularization factor'},
     '--epochs': {'default': 100, 'metavar': '-e', 'type': int, 'help': 'Number of training epochs'},
-    '--dropout': {'default': 1., 'metavar': '-d', 'type': float, 'help': 'Dropout regularization'},
-    '--momentum': {'default': 0.9, 'metavar': '-m', 'type': float, 'help': 'SGD Momentum'},
-    '--batch_size': {'default': 64, 'metavar': '-b', 'type': int, 'help': 'Batch size to train on'},
+    '--dropout': {'default': 0.41618, 'metavar': '-d', 'type': float, 'help': 'Dropout regularization'},
+    '--momentum': {'default': 0.742533, 'metavar': '-m', 'type': float, 'help': 'SGD Momentum'},
+    '--batch_size': {'default': 256, 'metavar': '-b', 'type': int, 'help': 'Batch size to train on'},
     '--batch_norm': {'default': True, 'metavar': '-n', 'type': bool, 'help': 'Enable or disable batch normalization'},
-    '--weight_decay': {'default': 5e-4, 'type': float, 'help': 'Weight decay (L2 regularization)'},
-    '--layers': {'default': [128] * 5, 'type': int, 'help': 'Fully connected hidden layer sizes', 'nargs': '+'},
+    '--weight_decay': {'default': 0.02985, 'type': float, 'help': 'Weight decay (L2 regularization)'},
+    '--layers': {'default': [256] * 5, 'type': int, 'help': 'Fully connected hidden layer sizes', 'nargs': '+'},
     '--save_dir': {'default': '/home/pes/deeplearning/models/generalization_training/train_1/', 'type': str, 'help': 'Tensorflow model save directory'}
 }
+
 
 INFERENCE_BATCH_SIZE = 1024
 ALLOW_GPU_MEM_GROWTH = True
@@ -34,7 +35,7 @@ N_CLASSES = 100 if USE_CIFAR100 else 10
 
 def main():
     # Parse cmd arguments
-    hp = utils.hyperparameters_from_args(hyperparameters_config, description='Fully connected neural network training on CIFAR-100')
+    hp = vars(utils.hyperparameters_from_args(hyperparameters_config, description='Fully connected neural network training on CIFAR-100'))
     dataset = utils.load_cifar(USE_CIFAR100)
     ops = build_graph(hp)
     train(hp, dataset, ops)
@@ -48,6 +49,8 @@ def train(hp, dataset, ops):
         # Initialize parameters and create summary writer
         best_acc = 0.
         sess.run(init_ops)
+        if hp['save_dir'][-1] != '/':
+            hp['save_dir'] += '/'
         shutil.rmtree(hp['save_dir'], ignore_errors=True)
         os.makedirs(hp['save_dir'], exist_ok=True)
         summary_writer = tf.summary.FileWriter(hp['save_dir'], sess.graph)

src/utils.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """ Utils """
+import sys
 import argparse
+import importlib
 import numpy as np
 import tensorflow as tf
+from collections import OrderedDict
+import tensorflow.contrib.graph_editor as ge
+from tensorflow.python.client import device_lib
 from tensorflow.python.keras.datasets import cifar10, cifar100
 
-__all__ = ['tf_config', 'num_flat_features', 'hyperparameters_from_args', 'default_hyperparameters', 'batch',
-           'add_summary_values', 'relu_xavier_avg', 'tanh_xavier_avg', 'linear_xavier_avg', 'leaky_relu', 'load_cifar']
+__all__ = ['import_and_reload', 'tf_config', 'get_available_gpus', 'num_flat_features', 'hyperparameters_from_args', 'default_hyperparameters', 'batch',
+           'add_summary_values', 'relu_xavier_avg', 'tanh_xavier_avg', 'linear_xavier_avg', 'leaky_relu', 'load_cifar', 'graph_replace', 'extract_update_dict']
 
 CIFAR100_MEAN = (0.5071, 0.4867, 0.4408)
 CIFAR100_STD = (0.2675, 0.2565, 0.2761)
@@ -25,10 +30,24 @@ tanh_xavier_avg = tf.variance_scaling_initializer(TANH_XAVIER_SCALE, mode="fan_a
 linear_xavier_avg = tf.variance_scaling_initializer(LINEAR_XAVIER_SCALE, mode="fan_avg")
 
 
-def tf_config(allow_growth=True, **kwargs):
+def import_and_reload(module_name, path='.'):
+    if path not in sys.path:
+        sys.path.append(path)
+    module = importlib.import_module(module_name)
+    module = importlib.reload(module)
+    return module
+
+
+def tf_config(allow_growth=True, allow_soft_placement=True, **kwargs):
     config = tf.ConfigProto(**kwargs)
     config.gpu_options.allow_growth = allow_growth
+    config.allow_soft_placement = allow_soft_placement
     return config
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 
 def leaky_relu(x, leak=0.2, name='leaky_relu'):
@@ -73,14 +92,16 @@ def num_flat_features(x, ignore_first_dim=True):
     return np.prod([dim for dim in shape])
 
 
-def batch(batch_size, *arrays, shuffle=False):
+def batch(batch_size, *arrays, shuffle=False, fixed_size_batches=False):
     """ Yields step number, effective batch size and arrays batches """
     length = len(arrays[0])
     if shuffle:
         perm = np.random.permutation(length)
         arrays = [array[perm] for array in arrays]
     batch_per_epoch = int(np.ceil(length / batch_size))
-    for step, range_min in zip(range(batch_per_epoch), range(0, length - 1, batch_size)):
+    for step, range_min in zip(range(batch_per_epoch), range(0, length, batch_size)):
+        if fixed_size_batches and range_min + batch_size > length:
+            break
         range_max = min(range_min + batch_size, length)
         yield step, range_max - range_min, (array[range_min:range_max] for array in arrays)
 
@@ -90,8 +111,43 @@ def load_cifar(load_cifar100=False):
     def _normalize(samples):
         v_min = samples.min(axis=(0, 1, 2), keepdims=True)
         v_max = samples.max(axis=(0, 1, 2), keepdims=True)
-        return (samples - v_min)/(v_max - v_min)
+        return (samples - v_min) / (v_max - v_min)
     (train_x, train_y), (test_x, test_y) = cifar100.load_data(label_mode='fine') if load_cifar100 else cifar10.load_data()
     (train_x, train_y) = np.reshape(_normalize(train_x), [-1, CIFAR_INPUT_SIZE]), np.reshape(train_y, [-1])
     (test_x, test_y) = np.reshape(_normalize(test_x), [-1, CIFAR_INPUT_SIZE]), np.reshape(test_y, [-1])
     return (train_x, train_y), (test_x, test_y)
+
+
+def graph_replace(*args, **kwargs):
+    """
+    Monkey patch graph_replace so that it works with TF 1.0+
+    See https://github.com/tensorflow/tensorflow/issues/9978 or https://github.com/tensorflow/tensorflow/issues/9125
+    """
+    for op in tf.get_default_graph().get_operations():
+        op._original_op = None
+    return ge.graph_replace(*args, **kwargs)
+
+
+def extract_update_dict(update_ops):
+    """Extract variables and their new values from Assign and AssignAdd ops.
+    Code from https://github.com/poolio/unrolled_gan/blob/master/Unrolled%20GAN%20demo.ipynb
+
+    Args:
+        update_ops: list of Assign and AssignAdd ops, typically computed using Keras' opt.get_updates()
+
+    Returns:
+        dict mapping from variable values to their updated value
+    """
+    name_to_var = {v.name: v for v in tf.global_variables()}
+    updates = OrderedDict()
+    for update in update_ops:
+        var_name = update.op.inputs[0].name
+        var = name_to_var[var_name]
+        value = update.op.inputs[1]
+        if update.op.type == 'Assign':
+            updates[var.value()] = value
+        elif update.op.type == 'AssignAdd':
+            updates[var.value()] = var + value
+        else:
+            raise ValueError("Update op type (%s) must be of type Assign or AssignAdd" % update_op.op.type)
+    return updates